92e62a23c5468406a3dacdb8202c970d5f4d923e
Jay <22547630+jPleyte@users.noreply.github.com>
2026-01-02 18:54:17 -0800

    Update setup, and add missing annovar script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 
 # Update and install system dependencies
 sudo apt-get update
-sudo apt-get install -y samtools perl wget tabix libpq-dev
+sudo apt-get install -y samtools perl wget tabix libpq-dev awscli
 
 /opt/conda/bin/pip install psycopg2-binary hgvs pysam biopython
 
@@ -10,9 +10,10 @@ mkdir -p data
 
 if [ ! -f data/Homo_sapiens_assembly19.fasta ]; then
     echo "Downloading hg19... this may take a moment."
-    wget -P data/ https://storage.googleapis.com/gatk-legacy-bundles/b37/Homo_sapiens_assembly19.fasta
-    wget -P data/ https://storage.googleapis.com/gatk-legacy-bundles/b37/Homo_sapiens_assembly19.fasta.fai
-    wget -P data/ https://storage.googleapis.com/gatk-legacy-bundles/b37/Homo_sapiens_assembly19.dict
+    
+    aws s3 cp s3://broad-references/hg19/v0/Homo_sapiens_assembly19.fasta data/ --no-sign-request
+    aws s3 cp s3://broad-references/hg19/v0/Homo_sapiens_assembly19.fasta.fai data/ --no-sign-request
+    aws s3 cp s3://broad-references/hg19/v0/Homo_sapiens_assembly19.dict data/ --no-sign-request    
 fi
 
 # Update Nextflow
@@ -22,3 +23,9 @@ nextflow -version
 # Annovar
 chmod +x annovar/*.pl
 echo "Before you can use annovar you must download the annovar database files"
+perl annovar/annotate_variation.pl -buildver hg19 -downdb -webfrom annovar refGeneWithVer annovar/humandb/
+# perl annovar/annotate_variation.pl -buildver hg19 -downdb -webfrom annovar refGeneWithVerMRNA annovar/humandb/
+
+# Customise the terminal command prompt
+printf "export PS1='\\[\\e[3;36m\\]\${PWD#/workspaces/} ->\\[\\e[0m\\] '\n" >> $HOME/.bashrc
+export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
